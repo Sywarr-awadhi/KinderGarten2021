bea257053a308224c1bd31b50c4392621ad689d1
Sywarr-awadhi <57920802+Sywarr-awadhi@users.noreply.github.com>
2021-03-06 18:12:26 +0100

    First commit

---
+++ src/main/java/tn/esprit/spring/KinderGarten2021Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinderGarten2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinderGarten2021Application.class, args);
+	}
+
+}
